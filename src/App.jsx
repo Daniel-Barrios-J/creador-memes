5d28ba908cfedd68b86a718cdf345f88c242aa2f
Daniel Barrios J <daniel.bj.rd@gmail.com>
2023-04-25 17:25:27 -0600

    styles:minor

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react';
 import html2canvas from 'html2canvas';
 import './App.css';
+import { imagesMemes } from './utils/templatesMemes';
 
 function App() {
 
   const [linea1, setLinea1] = useState('');
   const [linea2, setLinea2] = useState('');
-  const [imagen, setImagen] = useState('aliens');
+  const [imagen, setImagen] = useState(imagesMemes[0].url);
 
   const onChangelinea1 = (evento) => {
     const texto = evento.target.value;
@@ -22,29 +23,28 @@ function App() {
     setImagen(imagen);
   }
 
-  const onclickExport = (evento) => {
-    html2canvas(document.querySelector("#contenedorMeme")).then(canvas => { 
-      let img = canvas.toDataURL('image/png');
+  const onclickExport = () => {
+    html2canvas(document.getElementById('contenedorMeme')).then(canvas => { 
       let link = document.createElement('a');
-      link.download = `${imagen}.jpg`;
-      link.href = img
+      link.href = canvas.toDataURL();
+      link.download = `meme.png`;
       link.click();
-  });
+  }).catch(err => console.error('error al generar la imagen',err))
   }   
 
   return (
     <div className="App">
       <h1>Crea tu meme!</h1>
       <select name="opciones" id="" onChange={onChangeImagen}>
-        <option value="aliens">Aliens</option>
-        <option value="fry">Futurama</option>
-        <option value="smartguy">Smart guy</option>
+        {
+          imagesMemes.map(template => <option key={template.name} value={template.url}>{template.name}</option>)
+        }
       </select>
       <input type='text' placeholder='Texto superior' onChange={onChangelinea1}></input> 
       <div className='contenedorMeme' id='contenedorMeme'>
         <label className='linea1'>{linea1}</label> 
         <label className='linea2'>{linea2}</label> 
-        <img src={`/img/${imagen}.jpg`} alt={imagen} /> 
+        <img src={imagen} alt={imagen} /> 
       </div>
       <input type='text' placeholder='Texto inferior' onChange={onChangelinea2}></input>
       <button onClick={onclickExport}>Descargar</button>
---
+++ src/utils/templatesMemes.js
@@ -0,0 +1,34 @@
+export const imagesMemes = [
+  {
+    name: 'Fry',
+    url: 'https://i.pinimg.com/736x/3b/10/ec/3b10ec0ddfb39f0b479a1c97d2f883f5.jpg'
+  },
+  {
+    name: 'Aliens',
+    url: 'https://imgflip.com/s/meme/Ancient-Aliens.jpg'
+  },
+  {
+    name: 'Smart guy',
+    url: 'https://i.imgflip.com/1j8x99.jpg?a467136'
+  },
+  // {
+  //   name: 'Drake',
+  //   url: 'https://i.imgflip.com/4/30b1gx.jpg'
+  // },
+  // {
+  //   name: 'Spending Bob',
+  //   url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQuCOpJbj1pnBbWPJT4fbzzeIkvMcORbM6zsA&usqp=CAU'
+  // },
+  {
+    name: 'Same Spidey',
+    url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJa7NVdlirCtRKssQkFEHDomWVyOj9klEC4Q&usqp=CAU'
+  },
+  // {
+  //   name: 'This is fine',
+  //   url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQBnbNe0MgTM0a73cP8cIudXPA9OORv7tc7jw&usqp=CAU'
+  // },
+  // {
+  //   name: 'Choice',
+  //   url: 'https://i.imgflip.com/4/1g8my4.jpg'
+  // },
+]
